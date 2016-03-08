5fb66d62d53241ca2e93321f2cdcd96b01d63a27
Adam Smallcomb <atsmallcomb@gmail.com>
2016-03-08 16:52:32 -0500

    Form and pages

--- app.js
+++ app.js
@@ -13,43 +13,51 @@ var koa = require('koa');
 var _ = require('lodash');
 var app = koa();
 
-var fs = require('fs');
+var fs = require('co-fs');
+// var Promise = require('bluebird');
+// var fs = Promise.promisifyAll(require('fs'));
 // "database"
 
 var users = {}
 
 
 var groups = {};
+var picks = {};
+var games;
+var globalData = {};
+
 app.use(
   function *(next){
-users = {
-  'adam': {
-    'username': 'adam',
-    'scores': [92, 35, 112, 87, 79, 62, 79, 85, 53, 44, 72, 73, 95, 86, 125, 76, 99]
-  },
-  'chris': {
-    'username': 'chris',
-    'scores': [98, 50, 120, 95, 75, 59, 75, 81, 40, 33, 64, 76, 104, 89, 122, 74, 86]
-  },
-  'christine': {
-    'username': 'christine',
-    'scores': [100, 55, 123, 79, 71, 69, 79, 74, 48, 41, 67, 71, 84, 93, 129, 70, 86]
-  }
+    users = {
+      'adam': {
+        'username': 'adam',
+        'scores': [92, 35, 112, 87, 79, 62, 79, 85, 53, 44, 72, 73, 95, 86, 125, 76, 99]
+      },
+      'chris': {
+        'username': 'chris',
+        'scores': [98, 50, 120, 95, 75, 59, 75, 81, 40, 33, 64, 76, 104, 89, 122, 74, 86]
+      },
+      'christine': {
+        'username': 'christine',
+        'scores': [100, 55, 123, 79, 71, 69, 79, 74, 48, 41, 67, 71, 84, 93, 129, 70, 86]
+      }
 
-}
-groups = {'groups':[
-  {
-    'name': 'Us',
-    'members':['adam', 'chris']
-  },
-  {
-    'name': 'smallcombs',
-    'members': ['adam', 'chris', 'christine']
-  }
-]};
+    }
 
-  yield next;
-}
+    var paths = yield fs.readdir('./data');
+    var files = yield paths.map(function(path){
+      return {name: path.split('.json')[0], content: fs.readFile('data/' + path, 'utf8')};
+    });
+    yield files.map(function(content){
+     globalData[content.name] = JSON.parse(content.content);
+    })
+    globalData.weeks = [];
+    for(var i = 1;i<=17;i++ ){
+      globalData.weeks.push({week:i});
+    }
+    
+    yield next;
+  }
 )
 // var config = {};
 // middleware
@@ -68,7 +76,11 @@ app.use(handlebars({
 // })
 // route middleware
 
-app.use(route.get('/', list));
+app.use(route.get('/', season));
+app.use(route.get('/results/:week', results));
+app.use(route.get('/picks/:week', picker));
+
+
 app.use(route.get('/post/new', add));
 app.use(route.get('/data/:id', show));
 app.use(route.post('/post', create));
@@ -81,26 +93,80 @@ app.use(route.post('/data/:id', update));
  * Post listing.
  */
 
-function *list() {
-  groups.groups = groups.groups.map(function(group){
-    // console.log(group.members);
+function *season() {
+  // console.log(globalData.groups);
+  globalData.groups = globalData.groups.map(function(group){
     group.members = group.members.map(function(member){
-      // console.log(users[member]);
       var temp = users[member];
       temp.total = temp.scores.reduce(function(pv, cv) {
         return pv + cv;
       }, 0);
       return temp;
     })
-    // console.log(group);
     return group;
   })
-  yield this.render("season", groups);
-  // this.body = yield function *() {
-    // return handlebars.render('test', {});
-    // return fs.readdirSync(config.data_dir || './_site/data');
-  // };
+  yield this.render("season", globalData);
 }
+
+function *results(week) {
+  if(week == 'current')week = '1';
+  globalData.weeks[parseInt(week, 10)-1].current = true;
+  var games = _.filter(globalData.schedule, {'week': week}).map(function(game){
+    return _.pick(game,['id','winner']);
+  })
+  var ids = games.map(function(game){
+    return game.id;
+  })
+  globalData.groups = globalData.groups.map(function(group){
+    group.members = group.members.map(function(member){
+      var temp = users[member];
+      temp.games = globalData[member].filter(function(game){
+        return (ids.indexOf(game.id) >=0);
+      }).map(function(game){
+        game.winner = (_.find(games, {id: game.id}).winner === game.pick);
+        return game;
+      })
+      temp.total = temp.games.reduce(function(pv, cv, index, items) {
+        if(items[index].winner){
+          return pv + parseInt(cv.points, 10);
+        }
+        return pv;
+      }, 0);      
+      temp.wins = temp.games.reduce(function(pv, cv, index, items) {
+        if(items[index].winner){
+          return pv+ 1;
+        }
+        return pv;
+      }, 0);
+
+      return temp;
+    })
+    return group;
+  })
+  yield this.render("results", globalData);
+}
+
+function *picker(week) {
+  if(week == 'current')week = '1';
+  globalData.weeks[parseInt(week, 10)-1].current = true;
+  var games = _.filter(globalData.schedule, {'week': week}).map(function(game){
+    return _.pick(game,['id','home', 'away']);
+  })
+  var ids = games.map(function(game){
+    return game.id;
+  })
+  globalData.games = JSON.stringify(games);
+  yield this.render("picks", globalData);
+}
+
+
+
+
+
+
+
+
+
 
 /**
  * Show creation form.
@@ -121,7 +187,7 @@ function *show(id) {
   // this.body = yield render('show', { post: post });
 
   this.body = yield function *() {
-    return {data: JSON.parse(fs.readFileSync(config.data_dir + '/' + id + '.json', 'utf8')), schema: JSON.parse(fs.readFileSync(config.data_dir + '/schema/' + id + '.json', 'utf8'))};
+    // return {data: JSON.parse(fs.readFileSync(config.data_dir + '/' + id + '.json', 'utf8')), schema: JSON.parse(fs.readFileSync(config.data_dir + '/schema/' + id + '.json', 'utf8'))};
   };
 }
 
@@ -132,7 +198,7 @@ function *show(id) {
 function *update(id) {
 
   var data = yield parse(this);
-  fs.writeFileSync(config.data_dir + '/' + id + '.json', JSON.stringify(data));
+  // fs.writeFileSync(config.data_dir + '/' + id + '.json', JSON.stringify(data));
   this.body = yield function *() {
     return true;//JSON.parse(fs.readFileSync(config.data_dir + '/' + id + '.json', 'utf8'));
   };
--- assets/js/toolbox.js
+++ assets/js/toolbox.js
@@ -1,100 +1,105 @@
-// function render(template, data){
-// 	if(typeof templates[template] === 'undefined'){
-// 		templates[template] =  Hogan.compile($('#'+template).html());
-// 	}
-//   return templates[template].render(data, templates);
-// }
+function render(template, data){
+	if(typeof templates[template] === 'undefined'){
+		templates[template] =  Hogan.compile($('#'+template).html());
+	}
+  return templates[template].render(data, templates);
+}
 
 
 debug=true;
 alert = function(value){ if(debug){console.log(value);} };
-function render(template, data){
-	if(typeof templates[template] === 'undefined'){
-		if($('[name='+template+']').length > 0){
-			if(!$('[name='+template+']').attr('src')){
-				templates[template] =  Hogan.compile($('[name='+template+']').html());
-			}else{					
-				// $.get(, function (data) {
-				//   templates[template]= Hogan.compile(data);
-				// }, 'html')
 
-				jQuery.ajax({
-	        url: $('[name='+template+']').attr('src'),
-	        success: function (data) {
-						templates[template]= Hogan.compile(data);
-	        },
-	        async: false
-		    });
+
+
+window.onload =function(){
+	if($('#form').length){
+		$('#form').berry({default:{type: 'gamePick', min: 1, max: data.length}, fields: data });
+	}
+}
+
+
+var gamePick = `
+<div class="row clearfix {{modifiers}}" name="{{id}}" data-type="{{type}}" style="background:#fefefe;border-bottom:solid 1px #888;padding:5px">
+	<div class="col-md-5" style="text-align:right">
+		<div class="btn btn-default" data-value="{{{away}}}" style="width: 200px;line-height: 26px;">{{{away}}}<span class="cube team-icon {{away}}" style="margin:-2px;float:right"></span></div>
+	</div>
+	<div class="col-md-2" >
+	<select class="form-control"  name="{{id}}" {{^isEnabled}}readonly disabled="true"{{/isEnabled}}  {{#multiple_enable}}multiple{{/multiple_enable}} >
+	{{#options}}
+		<option {{#selected}}selected='selected'{{/selected}} value="{{value}}">
+			{{label}}
+		</option>
+	{{/options}}
+	</select>
+	</div>
+	<div class="col-md-5" style="text-align:left">
+		<div class="btn btn-default" data-value="{{{home}}}" style="width: 200px;line-height: 26px;"><span class="cube team-icon {{home}}" style="margin:-2px;float:left"></span>{{{home}}}</div>
+	</div>
+</div>`;
+
+templates['berry_gamePick'] = Hogan.compile(gamePick, templates);
+
+(function(b, $){
+	b.register({ type: 'gamePick',
+		create: function() {
+			this.options = b.processOpts.call(this.owner, this.item, this).options;
+			return b.render('berry_' + (this.elType || this.type), this);
+		},
+		defaults: {
+			selectedClass: 'btn-success',
+			defaultClass: 'btn-default',
+		},
+		setup: function() {
+			this.$el = this.self.find('select');
+			this.$el.off();
+			if(this.onchange !== undefined) {
+				this.on('change', this.onchange);
 			}
-			$('#'+template).remove();
-		}else{
-			return Hogan.compile(template).render(data, templates);	
+			this.$el.change($.proxy(function(){this.trigger('change');}, this));
+
+
+			// this.$el = this.self;
+			this.self.find('.btn').off();
+			this.self.find('.btn').on('click', $.proxy(function(e){
+				this.self.find('.' + this.selectedClass).toggleClass(this.selectedClass + ' ' + this.defaultClass);
+				$(e.target).closest('.btn').toggleClass(this.selectedClass + ' ' + this.defaultClass);
+				if(typeof this.onchange === 'function'){
+					this.onchange();
+				}
+				this.trigger('change');
+			}, this));
+
+
+
+		},
+		getValue: function() {
+			// var selected = this.self.find('[type="radio"]:checked').data('label');
+			// for(var i in this.item.options) {
+			// 	if(this.item.options[i].label == selected) {
+			// 		return this.item.options[i].value;
+			// 	}
+			// }
+			return {points: this.$el.val(), pick: this.self.find('.' + this.selectedClass).attr('data-value')}; 
+		},
+		setValue: function(value) {
+			this.value = value;
+			this.self.find('[value="' + this.value + '"]').prop('checked', true);
+		},
+		// 		getValue: function() {
+		// 	return this.$el.children('.' + this.selectedClass).attr('data-value');
+		// },
+		// setValue: function(val) {
+		// 	return this.$el.children('[data-value="'+val+'"]').click();
+		// },
+		displayAs: function() {
+			for(var i in this.item.options) {
+				if(this.item.options[i].value == this.lastSaved) {
+					return this.item.options[i].label;
+				}
+			}
+		},
+		focus: function(){
+			this.self.find('[type="radio"]:checked').focus();
 		}
-	}
-	if(typeof templates[template] !== 'undefined' && templates[template].length !== 0 ){
- 	 return templates[template].render(data, templates);
-	}else{
-		alert('not found:'+template);
-	}
-}
-
-
-
-function getNodeIndex(node) {
-  var index = 0;
-  while (node = node.previousSibling) {
-    if (node.nodeType != 3 || !/^\s*$/.test(node.data)) {
-      index++;
-    }
-  }
-  return index;
-}		
-function store(){
-	$.jStorage.set('session', JSON.stringify(session));
-}
-function clear(){
-	$.jStorage.set('session', JSON.stringify({}));
-	document.location.reload();
-}
-
-
-
-
-
-// var urlParams;
-var hashParams
-var QueryStringToHash = function QueryStringToHash  (query) {
-  var query_string = {};
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    pair[0] = decodeURIComponent(pair[0]);
-    pair[1] = decodeURIComponent((pair[1] || "").split('+').join(' '));
-      // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = pair[1];
-      // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]], pair[1] ];
-      query_string[pair[0]] = arr;
-      // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(pair[1]);
-    }
-  } 
-  return query_string;
-};
-// (window.onpopstate = function () {
-// 		hashParams = QueryStringToHash(document.location.hash.substr(1) || "")
-//     var match,
-//         pl     = /\+/g,  // Regex for replacing addition symbol with a space
-//         search = /([^&=]+)=?([^&]*)/g,
-//         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-//         query  = window.location.search.substring(1);
-
-//     urlParams = {};
-//     while (match = search.exec(query))
-//        urlParams[decode(match[1])] = decode(match[2]);
-// })();
-
-
+	});
+})(Berry, jQuery);
