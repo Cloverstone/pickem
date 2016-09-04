96511988314b44db432b9efafe709355cda2697d
Adam Smallcomb <atsmallcomb@gmail.com>
2016-09-04 15:53:43 -0400

    add comment input

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,9 @@
 window.onload =function(){
 	templates['comment'] = Hogan.compile(`<li  class="list-group-item">{{content}}!</li>`);
+	$('.form').berry({actions:false,fields: [{name:'content',label:false}]}).on('save',function(){
+		socket.emit('message', {group:'Us',content: this.toJSON().content});
+		this.fields.content.update({value:''});
+		this.fields.content.focus();
+
+	})
 }
